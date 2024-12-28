0fd06ed3c4e9520e8a21308d5ab7269ac120dd0f
yochan-stan <teganking5863@proton.me>
2024-12-28 14:41:10 -0600

    Add files via upload

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("peepee \\ poopoo!\n");
+    printf("i am finally learning how to do this shit\n");
+    printf("\t aw shit here we go again\n");
+    return 0;
+}
